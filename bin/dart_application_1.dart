7511f9ab881862650b9ae64862a28ec54ad79962
Nilavarasu2608 <knlsu444@gmail.com>
2023-04-11 18:50:04 +0530

    changes

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -7,15 +7,15 @@ import 'package:http/http.dart';
 
 void main(List<String> arguments) async {
   try {
-      var data = await getData();
-      var students = data['candidates'];
+    var data = await getData();
+    var students = data['candidates'];
 
-      for (var i = 0; i < students.length; i++) {
-        print(students[i]['name']);
-        print(students[i]['number']);
-      }
+    for (var i = 0; i < students.length; i++) {
+      print(students[i]['name']);
+      print(students[i]['number']);
+    }
   } catch (e) {
-    print("Failed to get data");
+    print("Failed to fetch data");
   }
 
   postData();
@@ -35,9 +35,8 @@ Future postData() async {
 }
 
 Future getData() async {
-  final response1 =
-      await get(Uri.parse("https://dl.dropboxusercontent.com/s/di27fjjj766ia8p/api.json?dl=0"));
-
+  final response1 = await get(Uri.parse(
+      "https://dl.dropboxusercontent.com/s/di27fjjj766ia8p/api.json?dl=0"));
 
   if (response1.statusCode == 200) {
     return jsonDecode(response1.body);
